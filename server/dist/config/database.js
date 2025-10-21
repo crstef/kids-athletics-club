d3f68c1b220a291981b189de0b0abc9c85687500
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-10-21 09:26:02 +0000

    🔄 Update backend build

--- server/dist/config/database.js
+++ server/dist/config/database.js
@@ -1,11 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+// Environment variables are already loaded by app.cjs before this module is imported
+// Just use them from process.env
 const pool = new pg_1.Pool({
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '5432'),
