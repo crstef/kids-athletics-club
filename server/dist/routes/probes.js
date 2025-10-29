7de79f477e5f976f6be7742ba69c844f7b1a2658
KidsAthleticsServer <admin@clubatletism.ro>
2025-10-29 18:48:06 +0000

    chore: update compiled server files

--- server/dist/routes/probes.js
+++ server/dist/routes/probes.js
@@ -1,9 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const probesController_1 = require("../controllers/probesController");
 const auth_1 = require("../middleware/auth");
-const authorizeDb_1 = require("../middleware/authorizeDb");
+const authorizeDb_1 = __importDefault(require("../middleware/authorizeDb"));
 const router = (0, express_1.Router)();
 router.use(auth_1.authenticate);
 router.get('/', (0, authorizeDb_1.default)('probes.view'), probesController_1.getAllProbes);
